8512db217355db6256b0f1f7424d9223b99971d4
Silvia Hernández Carrero <silvia.hdez.car@gmail.com>
2019-04-24 17:56:42 -0500

    IMPLEMENTACIÓN Y AJUSTES-Se implementó la vista de validaciones y tipificaciones -Se listan las validaciones en el recycler view -Se llena el autocomplete de tipificaciones -Se corrigieron las Ñ de observaciones. -Se modificó el inputData, porque esta sobreescribiendo, se creó uno nuevo -Se realizan mejoras en la vista de validaciones y tipificaciones

---
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/Adaptadores/AdapterTipificaciones.java
@@ -0,0 +1,64 @@
+package com.example.wposs_user.polariscoreandroid.Adaptadores;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wposs_user.polariscoreandroid.Comun.Global;
+import com.example.wposs_user.polariscoreandroid.R;
+import com.example.wposs_user.polariscoreandroid.java.Observacion;
+import com.example.wposs_user.polariscoreandroid.java.Validacion;
+
+import java.util.Vector;
+
+public class AdapterValidaciones extends RecyclerView.Adapter<AdapterValidaciones.ViewHolderValidaciones> {
+
+    private Vector<Validacion> listValidaciones;
+    private LayoutInflater inflador;
+
+    public AdapterValidaciones(Context c, Vector<Validacion> list) {
+        this.listValidaciones = list;
+        this.inflador = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public ViewHolderValidaciones onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.panel_validaciones, null);
+
+        return new ViewHolderValidaciones(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderValidaciones holder, int i) {
+       Validacion ob = this.listValidaciones.elementAt(i);
+        holder.txt_validaciones.setText(ob.getTeva_description());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listValidaciones.size();
+
+    }
+
+    public class ViewHolderValidaciones extends RecyclerView.ViewHolder {
+
+        TextView txt_validaciones;
+
+
+        public ViewHolderValidaciones(View v) {
+            super(v);
+            txt_validaciones = (TextView) v.findViewById(R.id.txt_validaciones);
+        }
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/java/Tabla.java
@@ -0,0 +1,4 @@
+package com.example.wposs_user.polariscoreandroid.java;
+
+public class Tabla {
+}
