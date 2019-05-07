7c6162ed87ad9b65d40143eaca1ecfcdf961413a
hender1001 <49322975+hender1001@users.noreply.github.com>
2019-05-07 16:58:34 -0500

    IMPLEMENTACION se implemento el stock para terminales asociadas a un tecnico.

--- app/src/main/java/com/example/wposs_user/polariscoreandroid/Fragmentos/StockFragment.java
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/Fragmentos/StockFragment.java
@@ -1,5 +1,6 @@
 package com.example.wposs_user.polariscoreandroid.Fragmentos;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -7,10 +8,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -22,6 +25,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.wposs_user.polariscoreandroid.Adaptadores.AdapterTerminal;
+import com.example.wposs_user.polariscoreandroid.Adaptadores.AdapterTerminalStock;
 import com.example.wposs_user.polariscoreandroid.Adaptadores.AdapterTerminal_asociada;
 import com.example.wposs_user.polariscoreandroid.Comun.Global;
 import com.example.wposs_user.polariscoreandroid.Comun.Messages;
@@ -29,6 +33,7 @@ import com.example.wposs_user.polariscoreandroid.Comun.Tools;
 import com.example.wposs_user.polariscoreandroid.Comun.Utils;
 import com.example.wposs_user.polariscoreandroid.R;
 import com.example.wposs_user.polariscoreandroid.TCP.TCP;
+import com.example.wposs_user.polariscoreandroid.java.Repuesto;
 import com.example.wposs_user.polariscoreandroid.java.Terminal;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -51,84 +56,141 @@ public class StockFragment extends Fragment {
 
 private View v;
 private ImageView foto_perfil;
-    private RequestQueue queue;
+ private RequestQueue queue;
+ private ArrayList<Terminal>terminales;
+private ArrayList<Repuesto>repuestos;
+Button rep;
+RecyclerView rv;
 
-    public StockFragment() {
-    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        queue = Volley.newRequestQueue(objeto);
-         v = inflater.inflate(R.layout.fragment_stock, container, false);
-         foto_perfil=(ImageView)v.findViewById(R.id.img_photo_perfil);
-         return v;
+          v = inflater.inflate(R.layout.fragment_stock, container, false);
+        queue = Volley.newRequestQueue(v.getContext());
+        rv= (RecyclerView)v.findViewById(R.id.recycler_stock);
+        rep=(Button)v.findViewById(R.id.btn_rep_stock);
+
+        rep.setOnClickListener(new View.OnClickListener() {
+               @Override
+            public void onClick(View v) {
+                  servicioTerminalStock();
+
+            }
+        });
+
+
+        return v;
 
     }
 
-    public void buscarFoto() {
-        Global.WEB_SERVICE = "/PolarisCore/upload/view/:1093.jpg ";
+    /********************************metodo usaddo para mostrar en stock las terminales y repuestos asignados a un tecnico****************/////////////
 
+    public void servicioTerminalStock(){
+    final  ArrayList<Terminal>terminales= new ArrayList<>();
+    final ArrayList<Repuesto>rep=new ArrayList<>();
+    final Gson gson = new GsonBuilder().create();
 
-
+    String url = "http://100.25.214.91:3000/PolarisCore/Terminals/stock";
+    JSONObject jsonObject = new JSONObject();
+        try {
+        jsonObject.put("user", Global.CODE);
+    } catch (JSONException e) {
+        e.printStackTrace();
     }
-
-
-    /**********************************Servicio para obtener la foto*///////////////////////////
-/*    public void consumirServicioFoto() {
-
-
-        final Gson gson = new GsonBuilder().create();
-        String url = "http://100.25.214.91:3000/PolarisCore/upload/view/:1093.jpg";
-         JSONObject jsonObject = new JSONObject();
-
-           JsonObjectRequest jsArrayRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                jsonObject,
-                new Response.Listener<Image>() {
-                    @Override
-                    public void onResponse(Image response) {
-
+    JsonObjectRequest jsArrayRequest = new JsonObjectRequest(
+            Request.Method.POST,
+            url,
+            jsonObject,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Global.STATUS_SERVICE = response.get("status").toString();
                         System.out.println("status:  " + Global.STATUS_SERVICE);
 
-                        System.out.println("status:  " + Global.STATUS_SERVICE);
-
-                        response = new JSONObject();
-
-
-
-                        System.out.println("status:  " + response.length());
+                        if (Global.STATUS_SERVICE.equals("fail")) {
+                            Global.mensaje = response.get("message").toString();
+                            Toast.makeText(v.getContext(),"Error:  "+Global.mensaje,Toast.LENGTH_SHORT).show();
+                            return;
+                        }
 
 
+                        response = new JSONObject(response.get("data").toString());
 
-                        System.out.println("status:  " + response.toString());
+
+                        JSONArray jsonArray1 = response.getJSONArray("terminales");
 
 
+                        if (jsonArray1.length() == 0) {
+                            Global.mensaje = "No tiene terminales asociadas";
+                            Toast.makeText(v.getContext(),Global.mensaje,Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        String ter = null;
+
+                        for (int i = 0; i < jsonArray1.length(); i++) {
+                            ter = jsonArray1.getString(i);
+
+                            Terminal t = gson.fromJson(ter, Terminal.class);
+                            if (t != null) {
+                            }
+                           terminales.add(t);
+                        }
+
+                        rv.setHasFixedSize(true);
+                        LinearLayoutManager llm = new LinearLayoutManager(Tools.getCurrentContext());
+                        rv.setLayoutManager(llm);
+                        AdapterTerminalStock adapter= new AdapterTerminalStock(v.getContext(),terminales);
+                        rv.setAdapter(adapter);
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-
-
-
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("ERROR", "Error Respuesta en JSON: " + error.getMessage());
-                        Toast.makeText(objeto, "ERROR\n " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                    Log.d("RESPUESTA", response.toString());
                 }
 
-        ) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                                return params;
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("ERROR", "Error Respuesta en JSON: " + error.getMessage());
+                    Toast.makeText(objeto, "ERROR\n " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
-        };
+
+    ) {
+        @Override
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("Authenticator", Global.TOKEN);
+
+            return params;
+        }
+    };
 
         queue.add(jsArrayRequest);
 
-    }*/
+}
+
+
+    public void llenarRVTerminalStock() {
+
+
+      {
+            rv.setHasFixedSize(true);
+            LinearLayoutManager llm = new LinearLayoutManager(Tools.getCurrentContext());
+            rv.setLayoutManager(llm);
+            ArrayList terminals = new ArrayList<>();
+          AdapterTerminalStock adapter= new AdapterTerminalStock(v.getContext(),terminales);
+          rv.setAdapter(adapter);
+        }
+    }
+
 
 
     }
