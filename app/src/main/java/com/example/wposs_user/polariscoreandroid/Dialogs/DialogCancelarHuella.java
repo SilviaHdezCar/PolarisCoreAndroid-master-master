10eec7858b3865cfe6c79280b3683867d49d7f47
hender1001 <49322975+hender1001@users.noreply.github.com>
2019-05-20 12:07:53 -0500

    IMPLEMENTACION servicio huella

---
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/Dialogs/DialogCancelarHuella.java
@@ -0,0 +1,54 @@
+package com.example.wposs_user.polariscoreandroid.Dialogs;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.wposs_user.polariscoreandroid.R;
+import com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass;
+
+import static com.example.wposs_user.polariscoreandroid.Actividades.MainActivity.objeto;
+import static com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass.eliminarValues;
+
+public class DialogCancelarHuella extends DialogFragment {
+
+    private View view;
+    private Button cancelar;
+    private Button aceptar;
+
+
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(getActivity());
+        view= getActivity().getLayoutInflater().inflate(R.layout.cancelar_huella,null);
+
+        cancelar=(Button)view.findViewById(R.id.btn_cancelarHuella);
+        aceptar=(Button)view.findViewById(R.id.btn_AceptarHuella);
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        dialogo.setView(view);
+        return dialogo.create();
+
+
+
+    }
+
+
+    public void desactivarHuella(){
+      // eliminarValuesLog(objeto);-->eliminar los valores de logueo por fecha
+    }
+
+
+
+}
