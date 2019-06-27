14e43796f1f20b7e1954caf2aa8256cbae784a06
Silvia Hernández Carrero <silviahernandez@wposs.com>
2019-06-27 08:17:41 -0500

    AJUSTES Autenticación con huella -Si la huella está deshabilitada, se muestra un mensaje

--- app/src/main/java/com/example/wposs_user/polariscoreandroid/Dialogs/DialogCancelarHuella.java
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/Dialogs/DialogCancelarHuella.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.wposs_user.polariscoreandroid.R;
 import com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass;
@@ -16,21 +17,30 @@ import com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass;
 import static com.example.wposs_user.polariscoreandroid.Actividades.MainActivity.objeto;
 import static com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass.eliminarValues;
 import static com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass.eliminarValuesLogueoHuella;
+import static com.example.wposs_user.polariscoreandroid.java.SharedPreferencesClass.getValueStrPreferenceLogueoHuella;
 
 public class DialogCancelarHuella extends DialogFragment {
 
     private View view;
     private Button cancelar;
     private Button aceptar;
+    private TextView text_mensaje;
 
 
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder dialogo = new AlertDialog.Builder(getActivity());
         view = getActivity().getLayoutInflater().inflate(R.layout.cancelar_huella, null);
 
+        text_mensaje = (TextView) view.findViewById(R.id.text_mensaje);
         cancelar = (Button) view.findViewById(R.id.btn_cancelarHuella);
         aceptar = (Button) view.findViewById(R.id.btn_AceptarHuella);
         setCancelable(false);
+
+        if(getValueStrPreferenceLogueoHuella(objeto,"email_user")==null
+                ||getValueStrPreferenceLogueoHuella(objeto,"email_user").isEmpty()){
+            text_mensaje.setText("El inicio de sesión mediante huella se encuentra deshabilitado");
+            aceptar.setVisibility(View.GONE);
+        }
 
         cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
